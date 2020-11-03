e2ec2d4cf381cb667d1b46c9b06ff6ea433736a5
cafehaine <kilian.guillaume@gmail.com>
2020-11-03 09:03:33 +0100

    Remove list copies for hill climber best

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -1,3 +1,4 @@
+import math
 from random import getrandbits, randrange, shuffle
 from typing import Collection, List, Tuple
 
@@ -5,7 +6,7 @@ class Knapsack:
     """A container for the required data to describe a knapsack situation."""
     def __init__(self, path: str):
         with open(path, "r") as data:
-            items = int(data.readline())
+            self.items = int(data.readline())
             self.profits = [int(elm) for elm in data.readline().split()]
             self.weights = [int(elm) for elm in data.readline().split()]
             self.max_weight = int(data.readline())
@@ -13,80 +14,61 @@ class Knapsack:
         self.beta = max([profit/weight for (profit, weight) in zip(self.profits, self.weights)])
 
 
-class Solution:
-    """A boolean mask to describe a knapsack solution."""
-    def __init__(self, knapsack: Knapsack, mask: List[bool]):
-        self.mask = mask
-        self.knapsack = knapsack
-        self.score = self._eval()
-
-    def _eval(self) -> float:
+    def evaluate(self, solution: List[bool]) -> float:
+        """Compute the score for a solution."""
         weight = 0
         profit = 0
-        for index, include in enumerate(self.mask):
+        for index, include in enumerate(solution):
             if include:
-                weight += self.knapsack.weights[index]
-                profit += self.knapsack.profits[index]
-        if weight >= self.knapsack.max_weight:
-            profit -= self.knapsack.beta * (weight - self.knapsack.max_weight)
+                weight += self.weights[index]
+                profit += self.profits[index]
+        if weight >= self.max_weight:
+            profit -= self.beta * (weight - self.max_weight)
 
         return profit
 
 
-def random_solution(knapsack: Knapsack) -> Solution:
+def random_solution(knapsack: Knapsack) -> List[bool]:
     """Return a random solution for a given knapsack."""
-    mask = [getrandbits(1) for i in range(len(knapsack.weights))]
-    return Solution(knapsack, mask)
+    return [bool(getrandbits(1)) for i in range(len(knapsack.weights))]
 
 
-def get_neighbours(base_solution) -> Collection[Solution]:
-    """Return all the neighbours for a given solution."""
-    output = []
-    for i in range(len(base_solution.mask)):
-        new_mask = base_solution.mask.copy()
-        new_mask[i] = not new_mask[i]
-        output.append(Solution(base_solution.knapsack, new_mask))
-    return output
-
-
-def get_random_neighbour(base_solution) -> Solution:
-    mask = base_solution.mask.copy()
-    index = randrange(len(mask))
-    mask[index] = not mask[index]
-
-    return Solution(base_solution.knapsack, mask)
-
-
-def hill_climber_best_improvement(knapsack: Knapsack) -> Tuple[Solution, int]:
+def hill_climber_best_improvement(knapsack: Knapsack) -> Tuple[float, int]:
     """Use the hill climber best improvement to find a solution."""
     base_solution = random_solution(knapsack)
+    base_score = knapsack.evaluate(base_solution)
     nb_eval = 1
 
     while True:
-        neighbours = get_neighbours(base_solution)
-        nb_eval += len(neighbours)
-        best = max(neighbours, key=lambda sol: sol.score)
-        if best.score > base_solution.score:
-            base_solution = best
+        best_score = -math.inf
+        index_solution = -1
+
+        for i in range(knapsack.items):
+            base_solution[i] = not base_solution[i]
+            score = knapsack.evaluate(base_solution)
+            nb_eval += 1
+
+            if score > best_score:
+                best_score = score
+                index_solution = i
+
+            base_solution[i] = not base_solution[i]
+
+        if best_score > base_score:
+            base_solution[index_solution] = not base_solution[index_solution]
+            base_score = best_score
         else:
             break # Optimum local
 
-    return base_solution, nb_eval
+    return base_score, nb_eval
 
 
-def get_neighbour(solution: Solution, index: int) -> Solution:
-    mask = solution.mask.copy()
-    mask[index] = not mask[index]
-    return Solution(solution.knapsack, mask)
-
-
-def hill_climber_first_improvement(knapsack: Knapsack, max_nb_eval: int) -> Solution:
+def hill_climber_first_improvement(knapsack: Knapsack, max_nb_eval: int) -> Tuple[float, int]:
     solution = random_solution(knapsack)
     nb_eval = 1
     voisins_possibles = [i for i in range(len(solution.mask))]
 
     while True:
-        print(solution.score, nb_eval)
         shuffle(voisins_possibles)
 
         index_voisin = 0
@@ -117,10 +99,10 @@ def hill_climber_first_improvement(knapsack: Knapsack, max_nb_eval: int) -> Solu
 def main():
     knapsack = Knapsack("./ks_1000.dat")
 
-    #solution_climber, evals = hill_climber_best_improvement(knapsack)
+    score, evals = hill_climber_best_improvement(knapsack)
+    print(score, evals)
+    #solution_climber, evals = hill_climber_first_improvement(knapsack, 200_000)
     #print(solution_climber.score, evals)
-    solution_climber, evals = hill_climber_first_improvement(knapsack, 200_000)
-    print(solution_climber.score, evals)
 
 
 if __name__ == '__main__':
