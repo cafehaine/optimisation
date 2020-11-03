87ced21cdd2ba5f0469676e81754f46d0e534c94
cafehaine <kilian.guillaume@gmail.com>
2020-11-03 09:19:10 +0100

    Remove list copies for hill climber first

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -64,45 +64,46 @@ def hill_climber_best_improvement(knapsack: Knapsack) -> Tuple[float, int]:
 
 
 def hill_climber_first_improvement(knapsack: Knapsack, max_nb_eval: int) -> Tuple[float, int]:
-    solution = random_solution(knapsack)
+    base_solution = random_solution(knapsack)
+    base_score = knapsack.evaluate(base_solution)
     nb_eval = 1
-    voisins_possibles = [i for i in range(len(solution.mask))]
+    voisins_possibles = [i for i in range(knapsack.items)]
 
     while True:
         shuffle(voisins_possibles)
+        current_score = base_score
+        index_voisin = -1
 
-        index_voisin = 0
-
-        x_prime = get_neighbour(solution, voisins_possibles[index_voisin])
-        nb_eval += 1
-        index_voisin += 1
-
-        while index_voisin < len(voisins_possibles) and solution.score > x_prime.score and nb_eval <= max_nb_eval:
-            x_prime = get_neighbour(solution, voisins_possibles[index_voisin])
-            nb_eval += 1
+        while index_voisin + 1 < len(voisins_possibles) and current_score <= base_score and nb_eval <= max_nb_eval:
             index_voisin += 1
+            index = voisins_possibles[index_voisin]
+            base_solution[index] = not base_solution[index]
+            current_score = knapsack.evaluate(base_solution)
+            nb_eval += 1
+            base_solution[index] = not base_solution[index]
 
-        if solution.score < x_prime.score:
-            print(f"Found better score: {solution.score} < {x_prime.score}")
-            solution = x_prime
-        elif index_voisin == len(voisins_possibles):
+        if current_score > base_score:
+            print(f"Found a better solution: {current_score} > {base_score}")
+            base_score = current_score
+            index = voisins_possibles[index_voisin]
+            base_solution[index] = not base_solution[index]
+        elif index_voisin +1 == len(voisins_possibles):
             print("Out of neighbours")
             break # optimum local
 
         if nb_eval >= max_nb_eval:
             break
-    print(solution.score, nb_eval)
 
-    return solution, nb_eval
+    return base_score, nb_eval
 
 
 def main():
     knapsack = Knapsack("./ks_1000.dat")
 
-    score, evals = hill_climber_best_improvement(knapsack)
+    #score, evals = hill_climber_best_improvement(knapsack)
+    #print(score, evals)
+    score, evals = hill_climber_first_improvement(knapsack, 200_000)
     print(score, evals)
-    #solution_climber, evals = hill_climber_first_improvement(knapsack, 200_000)
-    #print(solution_climber.score, evals)
 
 
 if __name__ == '__main__':
